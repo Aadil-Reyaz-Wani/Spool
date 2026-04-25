39e7e74617ede2553d7a95d128ba8a36b9febc0f
Aadil-Reyaz-Wani <aadilreyaz924@gmail.com>
2026-04-25 17:59:01 +0530

    chore(version): pump version name and code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,8 +31,8 @@ android {
         applicationId = "com.aadil.spool"
         minSdk = 24
         targetSdk = 36
-        versionCode = 4
-        versionName = "1.1.0"
+        versionCode = 5
+        versionName = "1.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
